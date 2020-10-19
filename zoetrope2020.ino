87623dacd0a3248dd958f1373e144f2905082ee8
Liam <liam@aacl.co.nz>
2020-10-20 11:03:00 +1300

    feat: Adding in address Excel spreadsheets
    Adding in a spreadsheet that may be used to record the addresses for
    each of the loops to make constructing the matrix easier.
    Also few minor code tweaks.

--- zoetrope2020.ino
+++ zoetrope2020.ino
@@ -18,7 +18,7 @@
 
 #define GLOBAL_BRIGHTNESS    4          // LED brightness (0-255), defines the LED PWM duty cycle
 #define NUM_LED_PER_STRIP   30          // Max LED Circuits under test (New boards have 20 per metre)
-#define NUM_STRIPS          36
+#define NUM_STRIPS          24
 #define NUM_LEDS            (NUM_LED_PER_STRIP * NUM_STRIPS)
 
 #define NUM_LOOPS            4
@@ -40,7 +40,11 @@ uint16_t XY (uint16_t x, uint16_t y) {
   }
 
   const uint16_t XYTable[] = {
-    0,1,2,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30
+    0,1,2,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29
+    59,58,57,56,55,54,53,52,51,50,49,48,47,46,45,44,43,42,41,40,39,38,37,36,35,34,33,32,31,30,
+    60,61,62,63,64,65,66,67,68,69,70,71,72,73,74,75,76,77,78,79,80,81,82,83,84,85,86,87,88,89,
+    119,118,117,116,115,114,113,112,111,110,109,108,107,106,105,104,103,102,101,100,99,98,97,96,95,94,93,92,91,90
+
   };
 
   uint16_t i = (y * kMatrixWidth) + x;
