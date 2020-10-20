5c6e4c45660c6bf2a0b7df30b468c4089c231734
Liam <liam@aacl.co.nz>
2020-10-20 21:56:48 +1300

    refactor: Added a plaintext message sending function, to the Arduino

--- segment_tester/basic_serial_test/basic_serial_test.ino
+++ segment_tester/basic_serial_test/basic_serial_test.ino
@@ -3,10 +3,19 @@ void setup() {
   Serial.begin(19200);
 }
 byte command_read[8];
+const byte serial_preamble[8] = {0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff};
 void loop() {
   // put your main code here, to run repeatedly:
   if (Serial.available() > 0){
     Serial.readBytes(command_read, 8);//Serial.readStringUntil('\n');
     Serial.write(command_read, 8);
   }
+  send_plaintext("Hello World!");
+  delay(1000);
+}
+
+void send_plaintext(String message){
+  Serial.write(serial_preamble, 8);
+  String to_send = message + '\n';
+  Serial.print(to_send);
 }
