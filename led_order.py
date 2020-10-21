326bc5499049e1223a1fda8540ce5806c8ae821b
Daniel Newton <djpnewton@gmail.com>
2020-10-21 22:56:19 +1300

    fix XYTable led mapping (was bug in parsing CSV)

---
+++ led_order.py
@@ -0,0 +1,39 @@
+import csv
+
+LOOPS = 4
+LEDS_PER_STRIP = 30
+
+def decomment(csvfile):
+    for row in csvfile:
+        raw = row.split('#')[0].strip()
+        if raw: yield raw
+
+def translate_strip(physical_segment, direction, logical_segment):
+    leds = []
+    start = LEDS_PER_STRIP * logical_segment
+    if direction:
+        for i in range(LEDS_PER_STRIP):
+            leds.append(start + i)
+    else:
+        for i in range(LEDS_PER_STRIP-1, -1, -1):
+            leds.append(start + i)
+    return leds
+
+with open('led_order.csv') as csvfile:
+    led_table = []
+    reader = csv.reader(decomment(csvfile))
+    for row in reader:
+        led_table += translate_strip(int(row[0]), int(row[1]), int(row[2]))
+
+    output = ""
+    LOOP_SIZE = int(len(led_table) / LOOPS)
+    for i in range(LOOPS):
+        for j in range(LOOP_SIZE):
+            output += '%d, ' % led_table[i * LOOP_SIZE + j]
+        output += '\n'
+    print(output)
+            
+            
+
+
+
