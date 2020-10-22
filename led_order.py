a8f74e64b36f36a6557708c6b61f8aafb27dd91f
Daniel Newton <djpnewton@gmail.com>
2020-10-22 15:16:28 +1300

    - start work on extension strip (spacer strip to basically carry signal to 3rd sector) - add option to disable STEPPER '#define STEPPER' - bumped brightness to full

--- led_order.py
+++ led_order.py
@@ -2,6 +2,7 @@ import csv
 
 LOOPS = 4
 LEDS_PER_STRIP = 30
+EXTENSION_STRIP = 16
 
 def decomment(csvfile):
     for row in csvfile:
@@ -11,6 +12,8 @@ def decomment(csvfile):
 def translate_strip(physical_segment, direction, logical_segment):
     leds = []
     start = LEDS_PER_STRIP * logical_segment
+    if EXTENSION_STRIP != -1 and logical_segment >= EXTENSION_STRIP:
+        start += LEDS_PER_STRIP
     if direction:
         for i in range(LEDS_PER_STRIP):
             leds.append(start + i)
